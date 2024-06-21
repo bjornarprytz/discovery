99dd6e9fd398e98387cc2bf2daa6de323253445a
Bjørnar Prytz <bjornarprytz@gmail.com>
2024-06-22 01:22:56 +0200

    fixed a bug

--- src/autoload/game.gd
+++ src/autoload/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 class_name GameClass
 
+signal ready_to_move
 signal moved(prev_pos: int, current_pos: int, direction: Vector2, score_change: int)
 signal multiplier_changed(new_value: int)
 signal invalid_move()
@@ -22,20 +23,20 @@ const QUEST_DISTANCE_FACTOR: int = 2
 
 var current_quest: String
 var is_golden: bool = true:
-	set(value):
-		if (value != is_golden):
-			is_golden = value
-			golden_changed.emit(is_golden)
+    set(value):
+        if (value != is_golden):
+            is_golden = value
+            golden_changed.emit(is_golden)
 
 var quest_duration := 0
 var quest_cap := 0
 var current_pos := 0
 var score := 0
 var multiplier: int = 1:
-	set(value):
-		if (value != multiplier):
-			multiplier = value
-			multiplier_changed.emit(multiplier)
+    set(value):
+        if (value != multiplier):
+            multiplier = value
+            multiplier_changed.emit(multiplier)
 
 var up: MoveCandidate
 var right: MoveCandidate
@@ -43,167 +44,167 @@ var down: MoveCandidate
 var left: MoveCandidate
 
 class MoveCandidate:
-	var destination: int
-	var character: String
-	var state: CorpusClass.CharState
-	var direction: Vector2
+    var destination: int
+    var character: String
+    var state: CorpusClass.CharState
+    var direction: Vector2
 
-	func _init(target_idx: int, direction_: Vector2):
-		character = Corpus.get_char_at(target_idx)
-		destination = target_idx
-		state = Corpus.get_state(target_idx)
-		direction = direction_
+    func _init(target_idx: int, direction_: Vector2):
+        character = Corpus.get_char_at(target_idx)
+        destination = target_idx
+        state = Corpus.get_state(target_idx)
+        direction = direction_
 
 func start(corpus: String=""):
-	is_golden = true
-	score = 0
-	quest_duration = 0
-	multiplier = 1
-	Corpus.load_corpus(corpus)
+    is_golden = true
+    score = 0
+    quest_duration = 0
+    multiplier = 1
+    Corpus.load_corpus(corpus)
 
 func try_move(input: String) -> bool:
-	if (input.length() > 1):
-		return false
-	
-	_reset_word_state()
+    if (input.length() > 1):
+        return false
+    
+    _reset_word_state()
 
-	var visited = [up, right, down, left].filter(func(c: MoveCandidate): return c.state.visited)
-	var not_visited = [up, right, down, left].filter(func(c: MoveCandidate): return !c.state.visited)
-	
-	var invalid = not_visited.filter(func(c1: MoveCandidate): return not_visited.filter(func(c2: MoveCandidate): return c1.character.nocasecmp_to(c2.character) == 0).size() > 1)
-	var valid = not_visited.filter(func(c: MoveCandidate): return Corpus.is_char_valid(c.character)).filter(func(c1: MoveCandidate): return !invalid.any(func(c2: MoveCandidate): return c1.character.nocasecmp_to(c2.character) == 0))
-	
-	if valid.is_empty():
-		for d in invalid:
-			Corpus.get_state((d as MoveCandidate).destination).invalid_move = true
-		Game.invalid_move.emit()
-		Game.game_over.emit(Game.score)
+    var visited = [up, right, down, left].filter(func(c: MoveCandidate): return c.state.visited)
+    var not_visited = [up, right, down, left].filter(func(c: MoveCandidate): return !c.state.visited)
+    
+    var invalid = not_visited.filter(func(c1: MoveCandidate): return not_visited.filter(func(c2: MoveCandidate): return c1.character.nocasecmp_to(c2.character) == 0).size() > 1)
+    var valid = not_visited.filter(func(c: MoveCandidate): return Corpus.is_char_valid(c.character)).filter(func(c1: MoveCandidate): return !invalid.any(func(c2: MoveCandidate): return c1.character.nocasecmp_to(c2.character) == 0))
+    
+    if valid.is_empty():
+        for d in invalid:
+            Corpus.get_state((d as MoveCandidate).destination).invalid_move = true
+        Game.invalid_move.emit()
+        Game.game_over.emit(Game.score)
 
-	for candidate in valid:
-		if (input.nocasecmp_to(candidate.character) == 0):
-			var prev_pos = current_pos
-			var score_change = _visit(candidate.destination)
-			score += score_change
-			Game.moved.emit(prev_pos, current_pos, candidate.direction, score_change)
-			if (candidate.direction == Vector2.UP or candidate.direction == Vector2.DOWN):
-				_tick_quest_duration()
-			return true
-			
-	if invalid.any(func(cand: MoveCandidate): return input.nocasecmp_to(cand.character) == 0):
-		for d in invalid:
-			Corpus.get_state((d as MoveCandidate).destination).invalid_move = true
-		Game.invalid_move.emit()
-	
-	return false
+    for candidate in valid:
+        if (input.nocasecmp_to(candidate.character) == 0):
+            var prev_pos = current_pos
+            var score_change = _visit(candidate.destination)
+            score += score_change
+            Game.moved.emit(prev_pos, current_pos, candidate.direction, score_change)
+            if (candidate.direction == Vector2.UP or candidate.direction == Vector2.DOWN):
+                _tick_quest_duration()
+            return true
+            
+    if invalid.any(func(cand: MoveCandidate): return input.nocasecmp_to(cand.character) == 0):
+        for d in invalid:
+            Corpus.get_state((d as MoveCandidate).destination).invalid_move = true
+        Game.invalid_move.emit()
+    
+    return false
 
 func force_move(target_pos: int, first_move: bool):
-	var prev_pos = current_pos
-	var score_change = _visit(target_pos, first_move)
-	Game.moved.emit(prev_pos, current_pos, Vector2.ZERO, score_change)
+    var prev_pos = current_pos
+    var score_change = _visit(target_pos, first_move)
+    Game.moved.emit(prev_pos, current_pos, Vector2.ZERO, score_change)
 
 func _ready() -> void:
-	start()
-	Game.completed_word.connect(_on_word_complete)
+    start()
+    Game.completed_word.connect(_on_word_complete)
 
 func _reset_word_state():
-	var word = Corpus.get_word_of(current_pos) as CorpusClass.WordData
-	if (word != null and word.states.all(func(s: CorpusClass.CharState): return s.visited or s.cursor)):
-		for s in word.states:
-			s.completed_word = false
+    var word = Corpus.get_word_of(current_pos) as CorpusClass.WordData
+    if (word != null and word.states.all(func(s: CorpusClass.CharState): return s.visited or s.cursor)):
+        for s in word.states:
+            s.completed_word = false
 
 func _on_word_complete(_word: String, was_quest: bool):
-	if (was_quest):
-		_cycle_quest()
-	
+    if (was_quest):
+        _cycle_quest()
+    
 func _reset_quest_duration(distance: int):
-	quest_cap = distance + current_quest.length()
-	quest_duration = quest_cap
-	Game.quest_duration_tick.emit(quest_duration, quest_cap)
+    quest_cap = distance + current_quest.length()
+    quest_duration = quest_cap
+    Game.quest_duration_tick.emit(quest_duration, quest_cap)
 
 func _tick_quest_duration():
-	quest_duration -= 1
-	if (quest_duration <= 0):
-		is_golden = false
-		_cycle_quest()
-	Game.quest_duration_tick.emit(quest_duration, quest_cap)
+    quest_duration -= 1
+    if (quest_duration <= 0):
+        is_golden = false
+        _cycle_quest()
+    Game.quest_duration_tick.emit(quest_duration, quest_cap)
 
 func _visit(target_idx: int, first_move: bool=false) -> int:
-	if (!first_move):
-		var prev_state = Corpus.get_state(current_pos) as CorpusClass.CharState
-		prev_state.cursor = false
-		prev_state.visited = true
-	
-	var next_state = Corpus.get_state(target_idx) as CorpusClass.CharState
-	next_state.cursor = true
-	
-	current_pos = target_idx
-	var score_change = 1 # Base score for moving
-	if (first_move):
-		_cycle_quest()
+    if (!first_move):
+        var prev_state = Corpus.get_state(current_pos) as CorpusClass.CharState
+        prev_state.cursor = false
+        prev_state.visited = true
+    
+    var next_state = Corpus.get_state(target_idx) as CorpusClass.CharState
+    next_state.cursor = true
+    
+    current_pos = target_idx
+    var score_change = 1 # Base score for moving
+    if (first_move):
+        _cycle_quest()
 
-	var word = Corpus.get_word_of(target_idx) as CorpusClass.WordData
-	
-	if (word != null and word.states.all(func(s: CorpusClass.CharState): return s.visited or s.cursor)):
-		score_change += word.word.length()
-		score_change *= multiplier
-		var is_target = current_quest.nocasecmp_to(word.word) == 0
-		
-		for s in word.states:
-			s.completed_word = true
-			if (is_target):
-				s.quest = true
-		
-		if (is_target):
-			score_change *= QUEST_MULTIPLIER
-			is_golden = true
-		
-		if (is_golden):
-			multiplier += 1
-		Game.completed_word.emit(word.word, is_target)
-	
-	for c in [up, right, down, left]:
-		if (c != null):
-			c.state.invalid_move = false
-	
-	up = MoveCandidate.new(current_pos - Corpus.corpus_line_length, Vector2.UP)
-	right = MoveCandidate.new(current_pos + 1, Vector2.RIGHT)
-	down = MoveCandidate.new(current_pos + Corpus.corpus_line_length, Vector2.DOWN)
-	left = MoveCandidate.new(current_pos - 1, Vector2.LEFT)
+    var word = Corpus.get_word_of(target_idx) as CorpusClass.WordData
+    
+    if (word != null and word.states.all(func(s: CorpusClass.CharState): return s.visited or s.cursor)):
+        score_change += word.word.length()
+        score_change *= multiplier
+        var is_target = current_quest.nocasecmp_to(word.word) == 0
+        
+        for s in word.states:
+            s.completed_word = true
+            if (is_target):
+                s.quest = true
+        
+        if (is_target):
+            score_change *= QUEST_MULTIPLIER
+            is_golden = true
+        
+        if (is_golden):
+            multiplier += 1
+        Game.completed_word.emit(word.word, is_target)
+    
+    for c in [up, right, down, left]:
+        if (c != null):
+            c.state.invalid_move = false
+    
+    up = MoveCandidate.new(current_pos - Corpus.corpus_line_length, Vector2.UP)
+    right = MoveCandidate.new(current_pos + 1, Vector2.RIGHT)
+    down = MoveCandidate.new(current_pos + Corpus.corpus_line_length, Vector2.DOWN)
+    left = MoveCandidate.new(current_pos - 1, Vector2.LEFT)
 
-	return score_change
+    return score_change
 
 func _cycle_quest():
-	var next_quest: CorpusClass.WordData
+    var next_quest: CorpusClass.WordData
 
-	var vertical_distance: int
-	var horizontal_distance: int
+    var vertical_distance: int
+    var horizontal_distance: int
 
-	while (next_quest == null or next_quest.is_completed()):
-		vertical_distance = (randi() % 3) + 3
-		horizontal_distance = (randi() % 15) + 5
-		next_quest = _get_random_word(horizontal_distance, vertical_distance)
-
-	current_quest = next_quest.word.to_lower()
-	
-	_reset_quest_duration(vertical_distance * QUEST_DISTANCE_FACTOR)
-	Game.new_quest.emit(current_quest)
+    while (next_quest == null or next_quest.is_completed()):
+        vertical_distance = (randi() % 2) + 2
+        horizontal_distance = (randi() % 10) + 2
+        next_quest = _get_random_word(vertical_distance, horizontal_distance)
+    
+    current_quest = next_quest.word.to_lower()
+    
+    _reset_quest_duration(horizontal_distance + (vertical_distance * QUEST_DISTANCE_FACTOR))
+    Game.new_quest.emit(current_quest)
 
 func _get_random_word(vertical_distance: int, horizontal_distance: int) -> CorpusClass.WordData:
-	var target_pos := current_pos
+    var target_pos := current_pos
 
-	# Random quadrant
-	match randi() % 4:
-		0: # Up and left
-			target_pos -= vertical_distance * Corpus.corpus_line_length
-			target_pos -= horizontal_distance
-		1: # Up and right
-			target_pos += vertical_distance * Corpus.corpus_line_length
-			target_pos -= horizontal_distance
-		2: # Down and right
-			target_pos += vertical_distance * Corpus.corpus_line_length
-			target_pos += horizontal_distance
-		3: # Down and left
-			target_pos -= vertical_distance * Corpus.corpus_line_length
-			target_pos += horizontal_distance
+    # Random quadrant
+    match randi() % 4:
+        0: # Up and left
+            target_pos -= (vertical_distance * Corpus.corpus_line_length)
+            target_pos -= horizontal_distance
+        1: # Down and left
+            target_pos += (vertical_distance * Corpus.corpus_line_length)
+            target_pos -= horizontal_distance
+        2: # Down and right
+            target_pos += (vertical_distance * Corpus.corpus_line_length)
+            target_pos += horizontal_distance
+        3: # Up and right
+            target_pos -= (vertical_distance * Corpus.corpus_line_length)
+            target_pos += horizontal_distance
 
-	return Corpus.get_word_of(target_pos)
+    return Corpus.get_word_of(target_pos)
--- src/main.gd
+++ src/main.gd
@@ -19,105 +19,107 @@ var show_ui := true
 var quest_duration := 0
 
 func _ready() -> void:
-    cam.position = Corpus.font_size / 2
-    target_pos = cam.position
-    
-    Game.moved.connect(_move)
-    Game.invalid_move.connect(_on_invalid_move)
-    Game.game_over.connect(_game_over)
-    Game.completed_word.connect(_on_completed_word)
-    
-    get_tree().create_timer(10.0).timeout.connect(_show_tutorial, CONNECT_ONE_SHOT)
+	cam.position = Corpus.font_size / 2
+	target_pos = cam.position
+	
+	Game.moved.connect(_move)
+	Game.invalid_move.connect(_on_invalid_move)
+	Game.game_over.connect(_game_over)
+	Game.completed_word.connect(_on_completed_word)
+	
+	get_tree().create_timer(10.0).timeout.connect(_show_tutorial, CONNECT_ONE_SHOT)
 
-    Game.force_move(text_game.center_segment.start_index, true)
-    
-    Audio.play_main()
+	Game.force_move(text_game.center_segment.start_index, true)
+	Game.ready_to_move.emit()
+	
+	Audio.play_main()
 
 func _show_tutorial():
-    if (made_first_move):
-        return
-        
-    if tutorial != null:
-        tutorial.queue_free()
-    
-    tutorial = tutorial_spawner.instantiate() as TutorialUI
-    ui.add_child(tutorial)
-    tutorial.modulate.a = 0.0
-    text_game.force_refresh()
-    
-    var tween = create_tween()
-    tween.tween_property(tutorial, "modulate:a", 1.0, .69)
-    
+	if (made_first_move):
+		return
+		
+	if tutorial != null:
+		tutorial.queue_free()
+	
+	tutorial = tutorial_spawner.instantiate() as TutorialUI
+	ui.add_child(tutorial)
+	tutorial.modulate.a = 0.0
+	text_game.force_refresh()
+	
+	var tween = create_tween()
+	tween.tween_property(tutorial, "modulate:a", 1.0, .69)
+	
 func _hide_tutorial():
-    if (tutorial == null or tutorial.is_queued_for_deletion()):
-        return
-    var tween = create_tween()
-    tween.tween_property(tutorial, "modulate:a", 0.0, .69)
-    
-    await tween.finished
+	if (tutorial == null or tutorial.is_queued_for_deletion()):
+		return
+	var tween = create_tween()
+	tween.tween_property(tutorial, "modulate:a", 0.0, .69)
+	
+	await tween.finished
 
-    if tutorial != null and !tutorial.is_queued_for_deletion():
-        tutorial.stop()
-    text_game.force_refresh()
+	if tutorial != null and !tutorial.is_queued_for_deletion():
+		tutorial.stop()
+	text_game.queue_full_refresh()
 
 func _move(_prev_pos: int, _current_pos: int, direction: Vector2, score_change: int):
-    $Camera/Sounds/Click.play()
-    target_pos += direction * Corpus.font_size
-    if (camera_tween != null):
-        camera_tween.kill()
-    camera_tween = create_tween()
-    camera_tween.tween_property(cam, 'position', target_pos, .2)
-    camera_tween.tween_callback(_flair.bind(score_change))
+	$Camera/Sounds/Click.play()
+	target_pos += direction * Corpus.font_size
+	if (camera_tween != null):
+		camera_tween.kill()
+	camera_tween = create_tween()
+	camera_tween.tween_property(cam, 'position', target_pos, .2)
+	camera_tween.tween_callback(_flair.bind(score_change))
 
 func _on_invalid_move():
-    $Camera/Sounds/Denied.play()
+	$Camera/Sounds/Denied.play()
 
 func _on_completed_word(_word: String, was_quest: bool):
-    if (was_quest):
-        $Camera/Sounds/Quest.play()
-    else:
-        $Camera/Sounds/CompleteWord.play()
+	if (was_quest):
+		$Camera/Sounds/Quest.play()
+	else:
+		$Camera/Sounds/CompleteWord.play()
 
 func _flair(amount: int):
-    var f = flair.instantiate() as CPUParticles2D
-    add_child(f)
-    f.position = cam.position
-    f.amount = amount
-    f.emitting = true
-    await get_tree().create_timer(f.lifetime).timeout
-    f.queue_free()
+	var f = flair.instantiate() as CPUParticles2D
+	add_child(f)
+	f.position = cam.position
+	f.amount = amount
+	f.emitting = true
+	await get_tree().create_timer(f.lifetime).timeout
+	f.queue_free()
 
 func _game_over(_score: int):
-    $Camera/Sounds/Finished.play()
-    game_over = true
-    game_over_label.show()
-    game_over_label.modulate = Color(1, 1, 1, 0)
-    Engine.time_scale = 0.5
-    camera_tween = create_tween().set_ease(Tween.EASE_IN).set_parallel()
-    camera_tween.tween_property(cam, 'zoom', Vector2.ONE * 10.0, 1.8)
-    camera_tween.tween_property($Camera/Fade, 'color', Color.BLACK, 1.8)
-    camera_tween.tween_property(game_over_label, 'modulate:a', 1.0, 1.8)
-    camera_tween.tween_interval(3.0)
-    camera_tween.set_parallel(false)
-    camera_tween.tween_callback(_show_score)
+	$Camera/Sounds/Finished.play()
+	game_over = true
+	game_over_label.show()
+	game_over_label.modulate = Color(1, 1, 1, 0)
+	Engine.time_scale = 0.5
+	camera_tween = create_tween().set_ease(Tween.EASE_IN).set_parallel()
+	camera_tween.tween_property(cam, 'zoom', Vector2.ONE * 10.0, 1.8)
+	camera_tween.tween_property($Camera/Fade, 'color', Color.BLACK, 1.8)
+	camera_tween.tween_property(game_over_label, 'modulate:a', 1.0, 1.8)
+	camera_tween.tween_interval(3.0)
+	camera_tween.set_parallel(false)
+	camera_tween.tween_callback(_show_score)
 
 func _show_score():
-    game_over = false
-    Engine.time_scale = 1.0
-    get_tree().change_scene_to_packed(score_scene)
+	game_over = false
+	Engine.time_scale = 1.0
+	get_tree().change_scene_to_packed(score_scene)
 
 func _toggle_ui():
-    show_ui = !show_ui
-    ui.set_show(show_ui)
+	show_ui = !show_ui
+	ui.set_show(show_ui)
 
 func _unhandled_key_input(event: InputEvent) -> void:
-    if (!game_over and event.is_released()):
-        var key = event.as_text()
-        if (key == "Tab"):
-            _toggle_ui()
-        else:
-            if Game.try_move(key):
-                made_first_move = true
-                _hide_tutorial()
-    elif game_over and event.as_text() == "Space":
-        Engine.time_scale = 2.0
+	if (!game_over and event.is_released()):
+		var key = event.as_text()
+		if (key == "Tab"):
+			_toggle_ui()
+		else:
+			if Game.try_move(key):
+				made_first_move = true
+				_hide_tutorial()
+			Game.ready_to_move.emit()
+	elif game_over and event.as_text() == "Space":
+		Engine.time_scale = 2.0
